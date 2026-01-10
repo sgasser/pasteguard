eb09d8e1ba83993f1dae84f77116eab9ae4f9482
maximiliancw <wunderkind.serie-0f@icloud.com>
2026-01-10 19:16:55 +0100

    Implement new pattern detector and add corresponding SecretEntityType options

--- src/secrets/patterns/types.ts
+++ src/secrets/patterns/types.ts
@@ -8,7 +8,10 @@ export type SecretEntityType =
   | "API_KEY_AWS"
   | "API_KEY_GITHUB"
   | "JWT_TOKEN"
-  | "BEARER_TOKEN";
+  | "BEARER_TOKEN"
+  | "ENV_PASSWORD"
+  | "ENV_SECRET"
+  | "CONNECTION_STRING";
 
 export interface SecretsMatch {
   type: SecretEntityType;
